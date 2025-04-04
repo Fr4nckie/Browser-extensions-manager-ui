39a28cc1409c704c34e31e1eb10c61c561cafe38
Franckie <franckie.dev@gmail.com>
2025-04-04 14:19:07 +0300

    feat: nest component into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header from './components/Header/index.jsx'
+import SectionExtensions from './components/SectionExtensions/index.jsx'
+
 function App() {
     return (
         <div className="container">
-            <div>hello</div>
+            <Header />
+            <SectionExtensions />
         </div>
     )
 }
