42eec61cab7a5d8182fe20cab9ca6e6c82c3ed62
Franckie <franckie.dev@gmail.com>
2025-04-04 14:16:12 +0300

    feat: implement Filters component

---
+++ src/components/Filters/index.jsx
@@ -0,0 +1,12 @@
+import './filters.styles.css'
+function Filters() {
+    return (
+        <div className="filters">
+            <button className="filters_btn">all</button>
+            <button className="filters_btn">active</button>
+            <button className="filters_btn">inactive</button>
+        </div>
+    )
+}
+
+export default Filters
