daa149b91d598eb29af4df3130ec8dbfc02c7852
Franckie <franckie.dev@gmail.com>
2025-04-07 13:27:43 +0300

    feat: persist user-selected theme  using localStorage

--- src/hooks/useTheme.jsx
+++ src/hooks/useTheme.jsx
@@ -1,17 +1,29 @@
 /* eslint-disable react/prop-types */
-import { useCallback, useContext } from 'react'
+import { useCallback, useContext, useEffect } from 'react'
 import { useState } from 'react'
 import { createContext } from 'react'
 
+const getTheme = () => {
+    const stored = localStorage.getItem('theme')
+    if (stored) {
+        return JSON.parse(stored)
+    }
+    return 'light'
+}
+
 const ThemeContext = createContext({
-    theme: 'dark',
+    theme: getTheme(),
     toggleTheme: () => {}
 })
 
 export const ThemeContextProvider = ({ children }) => {
-    const [theme, setTheme] = useState('dark')
+    const [theme, setTheme] = useState(getTheme())
     const toggleTheme = useCallback(() => {
         setTheme(theme === 'light' ? 'dark' : 'light')
+    }, [theme])
+
+    useEffect(() => {
+        localStorage.setItem('theme', JSON.stringify(theme))
     }, [theme])
 
     return (
